3376c70b3eea1eed4d01d743240be9e1ef1ee0d7
E32220112Fajar <144647918+E32220112Fajar@users.noreply.github.com>
2023-10-18 13:21:51 +0700

    update menu_screen

--- lib/menu_screen.dart
+++ lib/menu_screen.dart
@@ -1,203 +1,164 @@
 import 'package:flutter/material.dart';
 
-class MenuScreen extends StatelessWidget {
-  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+void main() {
+  runApp(MyApp());
+}
 
-  void _handleLogout(BuildContext context) {
-    Navigator.of(context).pop();
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MenuScreen(),
+    );
+  }
+}
+
+class MenuScreen extends StatefulWidget {
+  @override
+  _MenuScreenState createState() => _MenuScreenState();
+}
+
+class _MenuScreenState extends State<MenuScreen> {
+  bool showSensors = false;
+  bool showControl = false;
+  bool showMonitor = false;
+
+  void toggleSensors() {
+    setState(() {
+      showSensors = !showSensors;
+      showControl = false;
+      showMonitor = false;
+    });
+  }
+
+  void toggleControl() {
+    setState(() {
+      showSensors = false;
+      showControl = !showControl;
+      showMonitor = false;
+    });
+  }
+
+  void toggleMonitor() {
+    setState(() {
+      showSensors = false;
+      showControl = false;
+      showMonitor = !showMonitor;
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      key: _scaffoldKey,
-      appBar: AppBar(
-        backgroundColor: Colors.white,
-        elevation: 0,
-        leadingWidth: 100,
-        title: Row(
-          children: [
-            Padding(padding: EdgeInsets.only(left: 1)),
-            Image.asset(
-              "assets/image1.png",
-              width: 135,
-            ),
-          ],
-        ),
-        actions: [
-          IconButton(
-            icon: Icon(
-              Icons.exit_to_app,
-              color: Color(0xFF4CAF50),
-            ),
-            onPressed: () {
-              _handleLogout(context);
-            },
-          ),
-        ],
-        leading: IconButton(
-          padding: EdgeInsets.only(right: 55),
-          icon: Icon(
-            Icons.menu,
-            color: Color(0xFF4CAF50),
-          ),
-          onPressed: () {
-            _scaffoldKey.currentState?.openDrawer();
-          },
-        ),
-      ),
-      drawer: Drawer(
-        child: ListView(
-          padding: EdgeInsets.zero,
-          children: <Widget>[
-            DrawerHeader(
-              decoration: BoxDecoration(
-                color: Color(0xFF4CAF50),
-                image: DecorationImage(
-                  image: AssetImage(
-                    'assets/bg_4.jpg',
-                  ),
-                  fit: BoxFit.cover,
-                ),
-              ),
-              child: Padding(
-                padding: const EdgeInsets.only(
-                  top: 20.0,
-                ),
-                child: Text(
-                  'Menu',
-                  style: TextStyle(
-                    color: Color.fromRGBO(255, 255, 255, 1),
-                    fontSize: 24,
-                  ),
-                ),
-              ),
-            ),
-            ListTile(
-              leading: Icon(Icons.home),
-              title: Text('Home'),
-              subtitle: Text('Deskripsi Home'),
-              onTap: () {},
-            ),
-            ListTile(
-              leading: Icon(Icons.settings),
-              title: Text('Settings'),
-              subtitle: Text('Deskripsi Settings'),
-              onTap: () {},
-            ),
-          ],
-        ),
-      ),
+      backgroundColor: Colors.white,
       body: Column(
-        children: [
-          Expanded(
-            child: Container(
-              decoration: BoxDecoration(
-                image: DecorationImage(
-                  image: AssetImage(
-                    "assets/bg_6.jpg",
-                  ),
-                  fit: BoxFit.cover,
+        children: <Widget>[
+          SizedBox(
+            width: 220,
+            height: 50,
+            child: Align(
+              alignment: Alignment.centerLeft,
+              child: Padding(
+                padding: const EdgeInsets.only(left: 100.0),
+                child: Image.asset(
+                  'assets/image1.png',
+                  fit: BoxFit.contain,
+                  width: double.infinity,
+                  height: double.infinity,
                 ),
-              ),
-              child: GridView.count(
-                crossAxisCount: 2,
-                padding: EdgeInsets.all(5.0),
-                crossAxisSpacing: 12.0,
-                mainAxisSpacing: 16.0,
-                children: <Widget>[
-                  buildSensorCard('Sensor Suhu', Icons.wb_sunny, '24°C'),
-                  buildSensorCard('Sensor Kelembapan', Icons.opacity, '70%'),
-                  buildGraphCard(),
-                  buildControlButton('Aktifkan Otomatis'),
-                  buildControlButton('Aktifkan Manual'),
-                ],
               ),
             ),
           ),
+          SizedBox(height: 20.0),
+          ElevatedButton(
+            onPressed: toggleSensors,
+            child: Text('Tampilkan Monitoring Sensors'),
+          ),
+          SizedBox(height: 10.0),
+          ElevatedButton(
+            onPressed: toggleControl,
+            child: Text('Tampilkan Control Buttons'),
+          ),
+          SizedBox(height: 10.0),
+          ElevatedButton(
+            onPressed: toggleMonitor,
+            child: Text('Tampilkan Monitoring Laporan'),
+          ),
+          SizedBox(height: 20.0),
+          if (showSensors)
+            Container(
+              margin: EdgeInsets.all(20.0),
+              padding: EdgeInsets.all(20.0),
+              decoration: BoxDecoration(
+                color: Colors.green,
+                borderRadius: BorderRadius.circular(10.0),
+              ),
+              child: Text(
+                'Monitoring Suhu, Kelembapan, dan Cahaya',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20.0,
+                ),
+              ),
+            ),
+          if (showControl)
+            Container(
+              margin: EdgeInsets.all(20.0),
+              padding: EdgeInsets.all(20.0),
+              decoration: BoxDecoration(
+                color: Colors.blue,
+                borderRadius: BorderRadius.circular(10.0),
+              ),
+              child: Text(
+                'Control Buttons (Otomatis/Manual)',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20.0,
+                ),
+              ),
+            ),
+          if (showMonitor)
+            Container(
+              margin: EdgeInsets.all(20.0),
+              padding: EdgeInsets.all(20.0),
+              decoration: BoxDecoration(
+                color: Colors.orange,
+                borderRadius: BorderRadius.circular(10.0),
+              ),
+              child: Text(
+                'Monitoring Laporan',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20.0,
+                ),
+              ),
+            ),
         ],
       ),
-    );
-  }
-
-  Widget buildSensorCard(String title, IconData icon, String value) {
-    return Card(
-      elevation: 5,
-      child: Padding(
-        padding: EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Icon(
-              icon,
-              size: 60,
-            ),
-            SizedBox(height: 10),
-            Text(
-              title,
-              style: TextStyle(
-                fontSize: 18,
-                fontWeight: FontWeight.bold,
+      bottomNavigationBar: BottomAppBar(
+        color: Color.fromARGB(255, 48, 184, 53),
+        child: Container(
+          height: 50.0,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: <Widget>[
+              IconButton(
+                icon: Icon(Icons.home),
+                onPressed: () {},
+                color: Colors.white,
               ),
-            ),
-            SizedBox(height: 10),
-            Text(
-              value,
-              style: TextStyle(
-                fontSize: 24,
-                fontWeight: FontWeight.bold,
+              IconButton(
+                icon: Icon(Icons.settings),
+                onPressed: () {},
+                color: Colors.white,
               ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-
-  Widget buildGraphCard() {
-    return Card(
-      elevation: 5,
-      child: Padding(
-        padding: EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Icon(
-              Icons.show_chart,
-              size: 60,
-            ),
-            SizedBox(height: 10),
-            Text(
-              'Grafik Sensor',
-              style: TextStyle(
-                fontSize: 18,
-                fontWeight: FontWeight.bold,
+              IconButton(
+                icon: Icon(Icons.monitor),
+                onPressed: () {},
+                color: Colors.white,
               ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-
-  Widget buildControlButton(String title) {
-    return ElevatedButton(
-      onPressed: () {
-        // Tambahkan logika kontrol di sini
-      },
-      style: ElevatedButton.styleFrom(
-        primary: Color(0xff4CAF50),
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(10.0),
-        ),
-        padding: EdgeInsets.all(12.0),
-      ),
-      child: Text(
-        title,
-        style: TextStyle(
-          fontSize: 16.0,
-          fontWeight: FontWeight.bold,
-          color: Colors.white,
+            ],
+          ),
         ),
       ),
     );
