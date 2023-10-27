58044235957e9088b833b5054db17d0186412138
E32220112Fajar <144647918+E32220112Fajar@users.noreply.github.com>
2023-10-27 21:15:30 +0700

    update all

--- lib/menu_screen.dart
+++ lib/menu_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:mqtt_client/mqtt_client.dart' as mqtt;
+import 'package:mqtt_client/mqtt_server_client.dart' as mqtt;
 
 void main() {
   runApp(MyApp());
@@ -19,6 +21,7 @@ class MenuScreen extends StatefulWidget {
 }
 
 class _MenuScreenState extends State<MenuScreen> {
+  late mqtt.MqttServerClient client;
   bool isLightOn = false;
 
   void toggleLight() {
@@ -27,94 +30,58 @@ class _MenuScreenState extends State<MenuScreen> {
     });
   }
 
+  void connect() async {
+    client = mqtt.MqttServerClient(
+        '192.168.187.37', '1883'); // Ganti dengan alamat broker MQTT Anda
+
+    client.port = 1883; // Port default MQTT
+
+    client.logging(on: true); // Aktifkan logging untuk debug
+
+    try {
+      await client.connect('liztric',
+          '123'); // Ganti dengan username dan password broker MQTT Anda
+    } catch (e) {
+      print('Exception: $e');
+      client.disconnect();
+    }
+
+    if (client.connectionStatus?.state == mqtt.MqttConnectionState.connected) {
+      print('Connected to broker');
+      client.subscribe('android_suhu', mqtt.MqttQos.atMostOnce);
+      client.subscribe('android_kelembapan', mqtt.MqttQos.atMostOnce);
+
+      client.updates?.listen((List<mqtt.MqttReceivedMessage> c) {
+        final mqtt.MqttPublishMessage message = c[0].payload;
+        final String payload = mqtt.MqttPublishPayload.bytesToStringAsString(
+            message.payload.message);
+
+        if (c[0].topic == 'suhu_topic') {
+          print('Suhu: $payload');
+          // Tambahkan logika untuk menangani data suhu di sini
+        } else if (c[0].topic == 'kelembapan_topic') {
+          print('Kelembapan: $payload');
+          // Tambahkan logika untuk menangani data kelembapan di sini
+        }
+      });
+    } else {
+      print('Connection failed');
+      client.disconnect();
+    }
+  }
+
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Center(
-            child: Container(
-              padding: EdgeInsets.symmetric(
-                  horizontal: 40,
-                  vertical: 20), // Padding lebih panjang kiri dan kanan
-              decoration: BoxDecoration(
-                color: Color(0xFF4CAF50),
-                borderRadius: BorderRadius.only(
-                  bottomLeft: Radius.circular(30),
-                  bottomRight: Radius.circular(30),
-                ),
-              ),
-              child: Column(
-                children: [
-                  Image.asset(
-                    'assets/image1.png',
-                    fit: BoxFit.contain,
-                    width: 100,
-                  ),
-                  SizedBox(
-                    height: 50,
-                    width: 100,
-                  ),
-                  Text(
-                    'Farm Smart Mobile',
-                    style: TextStyle(fontSize: 18, color: Colors.white),
-                  ),
-                ],
-              ),
-            ),
-          ),
-          SizedBox(height: 30),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              _buildSensorContainer(
-                Icons.thermostat_outlined,
-                'Suhu',
-                '28°C', // Tambahkan monitoring derajat di sini
-              ),
-              _buildSensorContainer(
-                Icons.opacity,
-                'Kelembapan',
-                '70%', // Tambahkan monitoring derajat di sini
-              ),
-            ],
-          ),
-          SizedBox(height: 30),
-          Container(
-            margin: EdgeInsets.symmetric(horizontal: 30),
-            child: ElevatedButton(
-              onPressed: toggleLight,
-              style: ElevatedButton.styleFrom(
-                primary: Color(0xFF4CAF50),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                padding: EdgeInsets.all(12),
-              ),
-              child: Row(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Icon(
-                    isLightOn ? Icons.lightbulb : Icons.lightbulb_outline,
-                    color: Colors.white,
-                  ),
-                  SizedBox(width: 10),
-                  Text(
-                    isLightOn ? 'Matikan Lampu' : 'Nyalakan Lampu',
-                    style: TextStyle(
-                      fontSize: 16,
-                      color: Colors.white,
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ),
-        ],
-      ),
-    );
+  void initState() {
+    super.initState();
+    connect();
+  }
+
+  @override
+  void dispose() {
+    if (client != null) {
+      client.disconnect();
+    }
+    super.dispose();
   }
 
   Widget _buildSensorContainer(IconData icon, String label, String value) {
@@ -149,6 +116,76 @@ class _MenuScreenState extends State<MenuScreen> {
           Text(
             value,
             style: TextStyle(fontSize: 16, color: Colors.grey),
+          ),
+        ],
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Align(
+            alignment: Alignment.topCenter,
+            child: Container(
+              padding: EdgeInsets.symmetric(horizontal: 40, vertical: 10),
+              child: Image.asset(
+                'assets/image1.png',
+                fit: BoxFit.contain,
+                width: 180,
+              ),
+            ),
+          ),
+          SizedBox(height: 30),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              _buildSensorContainer(
+                Icons.thermostat_outlined,
+                'Suhu',
+                '28°C',
+              ),
+              _buildSensorContainer(
+                Icons.opacity,
+                'Kelembapan',
+                '70%',
+              ),
+            ],
+          ),
+          SizedBox(height: 30),
+          Container(
+            margin: EdgeInsets.symmetric(horizontal: 30),
+            child: ElevatedButton(
+              onPressed: toggleLight,
+              style: ElevatedButton.styleFrom(
+                primary: Color(0xFF4CAF50),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                padding: EdgeInsets.all(12),
+              ),
+              child: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Icon(
+                    isLightOn ? Icons.lightbulb : Icons.lightbulb_outline,
+                    color: Colors.white,
+                  ),
+                  SizedBox(width: 10),
+                  Text(
+                    isLightOn ? 'Matikan Lampu' : 'Nyalakan Lampu',
+                    style: TextStyle(
+                      fontSize: 16,
+                      color: Colors.white,
+                    ),
+                  ),
+                ],
+              ),
+            ),
           ),
         ],
       ),
