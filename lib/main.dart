11ea556442b0da3e71dd5d8147dd0356371e5133
E32220112Fajar <144647918+E32220112Fajar@users.noreply.github.com>
2023-11-04 13:43:33 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -4,11 +4,7 @@ import 'login_screen.dart'; // Import file login_screen.dart
 import 'menu_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-
-  await Firebase.initializeApp();
-
+void main() {
   runApp(MyApp());
 }
 
