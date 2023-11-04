5ce09b1ea6bedf4adfc56de4804304d86431a07d
E32220112Fajar <144647918+E32220112Fajar@users.noreply.github.com>
2023-11-04 12:25:23 +0700

    update all

--- lib/main.dart
+++ lib/main.dart
@@ -3,17 +3,12 @@ import 'splash_screen.dart';
 import 'login_screen.dart'; // Import file login_screen.dart
 import 'menu_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
 
 void main() async {
-  var messagingSenderId;
-  var firebaseApp = await Firebase.initializeApp(
-    options: FirebaseOptions(
-        apiKey: "AIzaSyAYx5QY9K2wnnHAfhUGoNEHEKjDXpGlEFQ",
-        appId: "255221761620",
-        messagingSenderId: messagingSenderId,
-        projectId: "farmsmartmobile"),
-  );
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp();
+
   runApp(MyApp());
 }
 
