5f5425bf4de35b3e246cdf22ed237ecadabf4c9d
E32220112Fajar <144647918+E32220112Fajar@users.noreply.github.com>
2023-11-06 16:30:18 +0700

    update menu - firebase

--- lib/menu_screen.dart
+++ lib/menu_screen.dart
@@ -1,10 +1,8 @@
-import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_database/firebase_database.dart';
+import 'package:flutter/material.dart';
+import 'package:mqtt_client/mqtt_server_client.dart' as mqtt;
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+void main() {
   runApp(MyApp());
 }
 
@@ -17,109 +15,41 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class SuhuDisplay extends StatefulWidget {
-  const SuhuDisplay({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  State<SuhuDisplay> createState() => _SuhuDisplayState();
-}
-
-class _SuhuDisplayState extends State<SuhuDisplay> {
-  late DatabaseReference _dbref;
-  String databasejson = '';
-  var newAge;
-
-  @override
-  void initState() {
-    super.initState();
-    _dbref = FirebaseDatabase.instance.ref();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            buildText('Age is $databasejson'),
-            buildText('New Age is: $newAge'),
-            StreamBuilder(
-              stream: _dbref.onValue,
-              builder: (context, AsyncSnapshot snap) {
-                if (snap.hasData &&
-                    !snap.hasError &&
-                    snap.data.snapshot.value != null) {
-                  Map data = snap.data.snapshot.value;
-                  List item = [];
-                  data.forEach(
-                      (index, data) => item.add({"key": index, ...data}));
-                  return Expanded(
-                    child: ListView.builder(
-                      itemCount: item.length,
-                      itemBuilder: (context, index) {
-                        return ListTile(
-                          title: Text("Customer: ${item[index]['key']}"),
-                          subtitle:
-                              Text('Age: ${item[index]['age'].toString()}'),
-                          isThreeLine: true,
-                        );
-                      },
-                    ),
-                  );
-                } else {
-                  return const Center(child: Text("No data"));
-                }
-              },
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-
-  Text buildText(String s) {
-    return Text(
-      s,
-      style: const TextStyle(
-        fontSize: 20,
-        fontWeight: FontWeight.bold,
-      ),
-    );
-  }
-
-  void ageChange() {
-    _dbref
-        .child('customer1')
-        .child('age')
-        .onValue
-        .listen((DatabaseEvent event) {
-      Object? data = event.snapshot.value;
-      print('weight data: $data');
-      setState(() {
-        newAge = data;
-      });
-    });
-  }
-}
-
 class MenuScreen extends StatefulWidget {
   @override
   _MenuScreenState createState() => _MenuScreenState();
 }
 
 class _MenuScreenState extends State<MenuScreen> {
+  final DatabaseReference _databaseReference =
+      FirebaseDatabase().reference().child('farmsmart').child('Kelembapan');
+
+  String _lembapValue = "";
+  late mqtt.MqttServerClient client;
   bool isLightOn = false;
 
   void toggleLight() {
     setState(() {
       isLightOn = !isLightOn;
     });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    // Tidak perlu memanggil fetchData() di initState, karena sudah digunakan dalam FutureBuilder
+  }
+
+  Future<String> fetchData() async {
+    try {
+      DatabaseEvent eventlembap = await _databaseReference.once();
+      if (eventlembap.snapshot.value != null) {
+        return eventlembap.snapshot.value.toString();
+      }
+      return ""; // Jika data null, return string kosong
+    } catch (e) {
+      return ""; // Tangani error jika terjadi
+    }
   }
 
   Widget _buildSensorContainer(IconData icon, String label, String value) {
@@ -163,69 +93,91 @@ class _MenuScreenState extends State<MenuScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Align(
-            alignment: Alignment.topCenter,
-            child: Container(
-              padding: EdgeInsets.symmetric(horizontal: 40, vertical: 10),
-              child: Image.asset(
-                'assets/image1.png',
-                fit: BoxFit.contain,
-                width: 180,
-              ),
-            ),
-          ),
-          SizedBox(height: 30),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              _buildSensorContainer(
-                Icons.thermostat_outlined,
-                'Suhu',
-                '9°C',
-              ),
-              _buildSensorContainer(
-                Icons.opacity,
-                'Kelembapan',
-                '70%',
-              ),
-            ],
-          ),
-          SizedBox(height: 30),
-          Container(
-            margin: EdgeInsets.symmetric(horizontal: 30),
-            child: ElevatedButton(
-              onPressed: toggleLight,
-              style: ElevatedButton.styleFrom(
-                primary: Color(0xFF4CAF50),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                padding: EdgeInsets.all(12),
-              ),
-              child: Row(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Icon(
-                    isLightOn ? Icons.lightbulb : Icons.lightbulb_outline,
-                    color: Colors.white,
+      body: FutureBuilder<String>(
+        future: fetchData(),
+        builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(
+              child: CircularProgressIndicator(),
+            ); // Menampilkan indikator loading jika sedang menunggu data
+          } else if (snapshot.hasError) {
+            return Center(
+              child: Text('Error: ${snapshot.error}'),
+            );
+          } else {
+            if (snapshot.data != null && snapshot.data!.isNotEmpty) {
+              return ListView(
+                children: <Widget>[
+                  Align(
+                    alignment: Alignment.topCenter,
+                    child: Container(
+                      padding:
+                          EdgeInsets.symmetric(horizontal: 40, vertical: 10),
+                      child: Image.asset(
+                        'assets/image1.png',
+                        fit: BoxFit.contain,
+                        width: 180,
+                      ),
+                    ),
                   ),
-                  SizedBox(width: 10),
-                  Text(
-                    isLightOn ? 'Matikan Lampu' : 'Nyalakan Lampu',
-                    style: TextStyle(
-                      fontSize: 16,
-                      color: Colors.white,
+                  SizedBox(height: 30),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      _buildSensorContainer(
+                        Icons.thermostat_outlined,
+                        'Suhu',
+                        '28°C',
+                      ),
+                      _buildSensorContainer(
+                        Icons.opacity,
+                        'Kelembapan',
+                        '${snapshot.data}',
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 30),
+                  Container(
+                    margin: EdgeInsets.symmetric(horizontal: 30),
+                    child: ElevatedButton(
+                      onPressed: toggleLight,
+                      style: ElevatedButton.styleFrom(
+                        primary: Color(0xFF4CAF50),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        padding: EdgeInsets.all(12),
+                      ),
+                      child: Row(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          Icon(
+                            isLightOn
+                                ? Icons.lightbulb
+                                : Icons.lightbulb_outline,
+                            color: Colors.white,
+                          ),
+                          SizedBox(width: 10),
+                          Text(
+                            isLightOn ? 'Matikan Lampu' : 'Nyalakan Lampu',
+                            style: TextStyle(
+                              fontSize: 16,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ],
+                      ),
                     ),
                   ),
                 ],
-              ),
-            ),
-          ),
-        ],
+              );
+            } else {
+              return Center(
+                child: Text('Data tidak ditemukan'),
+              );
+            }
+          }
+        },
       ),
     );
   }
